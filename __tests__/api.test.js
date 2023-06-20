441a1ba275f80e5dd8805919a8dcbd7350a35378
Katherine <hurstkatherine@hotmail.co.uk>
2023-06-20 12:21:19 +0100

    reorder tests

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -21,46 +21,44 @@ describe("fetchSinglePokemonData", () => {
       expect(pokemon.types).toEqual(["grass", "poison"]);
     });
   });
-  describe("fetchMovebyID", () => {
-    test("return the correct amount of move ids ", () => {
-      return fetchSinglePokemonData(1).then((pokemon) => {
-        expect(pokemon.moveIds).toHaveLength(83);
-      });
+  test("return the correct amount of move ids ", () => {
+    return fetchSinglePokemonData(1).then((pokemon) => {
+      expect(pokemon.moveIds).toHaveLength(83);
     });
-    test("returned object should contain an array with a single move id if the pokemon has one move (ditto)", () => {
-      return fetchSinglePokemonData(132).then((pokemon) => {
-        expect(pokemon.moveIds).toEqual([144]);
-      });
+  });
+  test("returned object should contain an array with a single move id if the pokemon has one move (ditto)", () => {
+    return fetchSinglePokemonData(132).then((pokemon) => {
+      expect(pokemon.moveIds).toEqual([144]);
     });
-    test("returned object should contain an array of move ids", () => {
-      return fetchSinglePokemonData(11).then((pokemon) => {
-        expect(pokemon.moveIds).toEqual([81, 106, 334, 450, 527]);
+  });
+  test("returned object should contain an array of move ids", () => {
+    return fetchSinglePokemonData(11).then((pokemon) => {
+      expect(pokemon.moveIds).toEqual([81, 106, 334, 450, 527]);
+    });
+  });
+});
+
+describe("getMoveByMoveId", () => {
+  test("should return an object", () => {
+    return fetchMoveByMoveId(1).then((move) => {
+      expect(Array.isArray(move)).toBe(false);
+      expect(typeof move).toBe("object");
+    });
+  });
+  test("should return an object with correct keys", () => {
+    return fetchMoveByMoveId(1).then((move) => {
+      expect(move).toMatchObject({
+        id: expect.any(Number),
+        name: expect.any(String),
+        pp: expect.any(Number),
+        power: expect.any(Number),
+        description: expect.any(String),
       });
     });
   });
-
-  describe("getMoveByMoveId", () => {
-    test("should return an object", () => {
-      return fetchMoveByMoveId(1).then((move) => {
-        expect(Array.isArray(move)).toBe(false);
-        expect(typeof move).toBe("object");
-      });
-    });
-    test("should return an object with correct keys", () => {
-      return fetchMoveByMoveId(1).then((move) => {
-        expect(move).toMatchObject({
-          id: expect.any(Number),
-          name: expect.any(String),
-          pp: expect.any(Number),
-          power: expect.any(Number),
-          description: expect.any(String),
-        });
-      });
-    });
-    test('if move has no flavour text, replaces with "No description found"', () => {
-      return fetchMoveByMoveId(851).then((move) => {
-        expect(move.description).toBe("No description found");
-      });
+  test('if move has no flavour text, replaces with "No description found"', () => {
+    return fetchMoveByMoveId(851).then((move) => {
+      expect(move.description).toBe("No description found");
     });
   });
 });
